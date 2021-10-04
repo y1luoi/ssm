3590230304f14227e74e4e12e3b705154ef5dfc5
y1luo <y1luo9264@163.com>
2021-10-04 13:43:17 +0800

    first commit

---
+++ src/main/java/com/yiluo/pojo/Books.java
@@ -0,0 +1,20 @@
+package com.yiluo.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Books {
+
+    private int bookID;
+    private String bookName;
+    private int bookCounts;
+    private String detail;
+
+}
